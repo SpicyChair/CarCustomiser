0f477810bba01d85297ebf11f063a9a7c59d1738
Ethan-Scott Hin <hin.e@etoncollege.org.uk>
2022-01-14 18:58:51 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Hin, Ethan-Scott (WING) on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
