49c5ae8c5bb9da9e104219c8b9101416d8ac67f9
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-15 18:13:30 +0000

    Add Part 2

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let car = Car(make:"Porsche", model:"911 Turbo", topSpeed:200, acceleration:8.2, handling: 5)
-    
-    var body: some View {
-        VStack{
-            Text(car.displayStats())
+    @State private var currentCar:Int = 0 {
+        didSet {
+            if currentCar >= starterCars.cars.count {
+                currentCar = 0
+            }
         }
     }
+    @State private var exhaustPackage:Bool = false
+    @State private var tiresPackage:Bool = false
+    //the state determines what is shown on the screen
+    @State private var starterCars = StarterCars()
+    
+    var body: some View {
+        
+        let exhaustPackageBinding = Binding<Bool> (get: {exhaustPackage},
+                                                   set: {newValue in
+                                                    self.exhaustPackage = newValue
+                                                    if self.exhaustPackage {
+                                                        self.starterCars.cars[currentCar].topSpeed += 10
+                                                        self.starterCars.cars[currentCar].acceleration -= 0.3
+                                                        
+                                                    } else {
+                                                        self.starterCars.cars[currentCar].topSpeed -= 10
+                                                        self.starterCars.cars[currentCar].acceleration += 0.3
+                                                    }
+                                                    
+                                                   })
+        let tiresPackageBinding = Binding<Bool> (get: {tiresPackage},
+                                                 set: {newValue in
+                                                    self.tiresPackage = newValue
+                                                    if self.tiresPackage {
+                                                        self.starterCars.cars[currentCar].handling += 2
+                                                    } else {
+                                                        self.starterCars.cars[currentCar].handling -= 2
+                                                    }
+                                                 })
+        
+        Form {
+        VStack(alignment: .leading, spacing: 20){
+            Text(starterCars.cars[currentCar % 4].displayStats())
+            Button("Next Car", action:{
+                currentCar = currentCar + 1
+            })
+            
+            
+        }
+            Section {
+                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                Toggle("Racing Tires Package", isOn: tiresPackageBinding)
+            }
+        
+        }}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,19 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Hin, Ethan-Scott (WING) on 15/01/2022.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [
+        Car(make:"Porsche", model:"911 Turbo", topSpeed:200, acceleration:5.2, handling: 5),
+        Car(make:"Clarkson", model:"P45", topSpeed:40, acceleration:999, handling: 1),
+        Car(make:"Tesla", model:"Roadster 2022", topSpeed:220, acceleration:1.9, handling: 4),
+        Car(make:"Volkswagen", model:"Golf GTI", topSpeed:135, acceleration:8.3, handling: 7),
+        Car(make:"Renault", model:"Cilo", topSpeed:125, acceleration:9.2, handling: 8),
+        
+    ]
+}
