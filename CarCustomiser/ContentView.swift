a00f4914cafab425b7e51549a032a194df17f93f
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-19 10:01:34 +0000

    add part 3

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
         didSet {
             exhaustPackage = false
             tiresPackage = false
+            engineUpgrade = false
+            coins = 100
             
             if currentCar >= starterCars.cars.count {
                 currentCar = 0
@@ -20,13 +22,16 @@ struct ContentView: View {
         }
     }
     @State private var coins:Int = 100
-    let exhaustCost:Int = 65
-    let tiresCost:Int = 35
+    let exhaustCost:Int = 50
+    let tiresCost:Int = 30
+    let engineCost:Int = 75
     
     
     
     @State private var exhaustPackage:Bool = false
     @State private var tiresPackage:Bool = false
+    @State private var engineUpgrade:Bool = false
+    
     //the state determines what is shown on the screen
     @State private var starterCars = StarterCars()
     
@@ -34,49 +39,55 @@ struct ContentView: View {
         
         let exhaustPackageBinding = Binding<Bool> (get: {exhaustPackage},
                                                    set: {newValue in
-                                                    if self.coins >= self.exhaustCost || self.exhaustPackage {
-                                                        //check if user has enough funds or already owns package
-                                                        self.exhaustPackage = newValue
-                                                        if self.exhaustPackage {
-                                                            self.starterCars.cars[currentCar].topSpeed += 10
-                                                            self.starterCars.cars[currentCar].acceleration -= 0.3
-                                                            self.coins -= self.exhaustCost
-                                                            
-                                                        } else {
-                                                            self.starterCars.cars[currentCar].topSpeed -= 10
-                                                            self.starterCars.cars[currentCar].acceleration += 0.3
-                                                            self.coins += self.exhaustCost
-                                                        }
+                            
+                                                    self.exhaustPackage = newValue
+                                                    print(newValue)
+                                                    if self.exhaustPackage {
+                                                        self.starterCars.cars[currentCar].topSpeed += 10
+                                                        self.starterCars.cars[currentCar].acceleration -= 0.3
+                                                        self.coins -= self.exhaustCost
+                                                        
                                                     } else {
-                                                        Alert(
-                                                            title: Text("Error"),
-                                                            message: Text("Not enough funds!")
-                                                        )
-                                                        //do something with the alert
+                                                        self.starterCars.cars[currentCar].topSpeed -= 10
+                                                        self.starterCars.cars[currentCar].acceleration += 0.3
+                                                        self.coins += self.exhaustCost
                                                     }
+                                                   
                                                     
                                                    })
         let tiresPackageBinding = Binding<Bool> (get: {tiresPackage},
                                                  set: {newValue in
-                                                    if self.coins >= self.tiresCost || self.tiresPackage {
-                                                        //check if user has enough funds or already owns package
-                                                        self.tiresPackage = newValue
-                                                        if self.tiresPackage {
-                                                            self.starterCars.cars[currentCar].handling += 2
-                                                            self.coins -= self.tiresCost
-                                                        } else {
-                                                            self.starterCars.cars[currentCar].handling -= 2
-                                                            self.coins += self.tiresCost
-                                                        }
-                                                    } else {
-                                                        Alert(
-                                                            title: Text("Error"),
-                                                            message: Text("Not enough funds!")
-                                                        )
-                                                        //do something with the alert
-                                                    }
                                                     
+                                                    //check if user has enough funds or already owns package
+                                                    self.tiresPackage = newValue
+                                                    if self.tiresPackage {
+                                                        self.starterCars.cars[currentCar].handling += 2
+                                                        self.coins -= self.tiresCost
+                                                    } else {
+                                                        self.starterCars.cars[currentCar].handling -= 2
+                                                        self.coins += self.tiresCost
+                                                    }
+                                             
+                                                
                                                  })
+        let engineBinding = Binding<Bool> (get: {engineUpgrade},
+                                           set: {newValue in
+                                              
+                                              //check if user has enough funds or already owns package
+                                              self.engineUpgrade = newValue
+                                              if self.engineUpgrade {
+                                                    self.starterCars.cars[currentCar].topSpeed += 27
+                                                    self.starterCars.cars[currentCar].acceleration -= 0.7
+                                                    self.coins -= self.engineCost
+                                              } else {
+                                                    self.starterCars.cars[currentCar].topSpeed -= 27
+                                                    self.starterCars.cars[currentCar].acceleration += 0.7
+                                                    self.coins += self.engineCost
+                                              }
+                                       
+                                          
+                                           })
+                                            
         
         Form {
         VStack(alignment: .leading, spacing: 20){
@@ -88,8 +99,9 @@ struct ContentView: View {
             
         }
             Section {
-                Toggle("Exhaust Package (Costs \(exhaustCost))", isOn: exhaustPackageBinding)
-                Toggle("Racing Tires Package (Costs \(tiresCost))", isOn: tiresPackageBinding)
+                Toggle("Exhaust Package (Costs \(exhaustCost))", isOn: exhaustPackageBinding).disabled(exhaustCost > coins && !exhaustPackage)
+                Toggle("Racing Tires Package (Costs \(tiresCost))", isOn: tiresPackageBinding).disabled(tiresCost > coins && !tiresPackage)
+                Toggle("Engine Upgrade (Costs \(engineCost))", isOn: engineBinding).disabled(engineCost > coins && !engineUpgrade)
             }
             Section {
                 Text("Available Funds: \(coins)")
--- CarCustomiserUITests/CarCustomiserUITests.swift
+++ CarCustomiserUITests/CarCustomiserUITests.swift
@@ -9,27 +9,39 @@ import XCTest
 
 class CarCustomiserUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func CheckSwitchDisabledWhenOutOfFunds() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+                
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Exhaust Package (Costs 50)"]/*[[".cells[\"Exhaust Package (Costs 50)\"].switches[\"Exhaust Package (Costs 50)\"]",".switches[\"Exhaust Package (Costs 50)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Racing Tires Package (Costs 30)"]/*[[".cells[\"Racing Tires Package (Costs 30)\"].switches[\"Racing Tires Package (Costs 30)\"]",".switches[\"Racing Tires Package (Costs 30)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(!tablesQuery/*@START_MENU_TOKEN@*/.switches["Engine Upgrade (Costs 75)"]/*[[".cells[\"Engine Upgrade (Costs 75)\"].switches[\"Engine Upgrade (Costs 75)\"]",".switches[\"Engine Upgrade (Costs 75)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.isEnabled)
+    
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func CheckUpgradesCanBeResoldIfNoFunds() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+                
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Exhaust Package (Costs 50)"]/*[[".cells[\"Exhaust Package (Costs 50)\"].switches[\"Exhaust Package (Costs 50)\"]",".switches[\"Exhaust Package (Costs 50)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Racing Tires Package (Costs 30)"]/*[[".cells[\"Racing Tires Package (Costs 30)\"].switches[\"Racing Tires Package (Costs 30)\"]",".switches[\"Racing Tires Package (Costs 30)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Exhaust Package (Costs 50)"]/*[[".cells[\"Exhaust Package (Costs 50)\"].switches[\"Exhaust Package (Costs 50)\"]",".switches[\"Exhaust Package (Costs 50)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.switches["Racing Tires Package (Costs 30)"]/*[[".cells[\"Racing Tires Package (Costs 30)\"].switches[\"Racing Tires Package (Costs 30)\"]",".switches[\"Racing Tires Package (Costs 30)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    
+    
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
